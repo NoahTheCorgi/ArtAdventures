1951e2dbc1fcd018f6b770865a166dd457037f44
NoahTheCorgi <91349811+NoahTheCorgi@users.noreply.github.com>
2023-01-23 02:27:45 -0500

    dynamic ellipses paint brush tool version 1

--- p5/artToolV1.js
+++ p5/artToolV1.js
@@ -14,13 +14,17 @@ function keyPressed() {
   }
 }
 
+let counter = 0;
+
 function draw() {
   if (mouseIsPressed) {
     fill(color);
   } else {
     fill(255); // white
   }
-  ellipse(mouseX, mouseY, 100, 100);
+  ellipse(mouseX, mouseY, 100 + (counter - 15), 100 - (counter - 15));
   color += 5;
   color %= 256;
+  counter += 1;
+  counter %= 31;
 }
